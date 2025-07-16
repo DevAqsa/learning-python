3cbc029d2d9698739d9224c2186a452537056f86
Aqsa Mumtaz <codewithaqsa@gmail.com>
2025-07-16 19:59:55 +0500

    loops started

---
+++ loops.py
@@ -0,0 +1,17 @@
+# loops in python -- repeat the instructions 
+
+# while loop and for loop
+
+# count = 1
+# while count <= 5 :
+#     print("hello")
+#     count += 1
+
+#the variable with the help of we iterate is called iterator and the circle is called iteration
+
+
+# print 1 to 5 numbers
+i = 1 
+while i <= 5:
+    print(i)
+    i += 1
