ff81b544e9e4324ee9d7c23a1b190354733d708b
Aqsa Mumtaz <codewithaqsa@gmail.com>
2025-07-17 17:53:52 +0500

    practice questions

--- loops.py
+++ loops.py
@@ -11,7 +11,46 @@
 
 
 # print 1 to 5 numbers
-i = 1 
-while i <= 5:
-    print(i)
-    i += 1
+# i = 1 
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+
+# practice questions 
+
+# q no 1 
+
+# i = 1
+
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# q no 2
+
+# i = 100
+
+# while i >=1:
+#     print(i)
+#     i -= 1
+
+# q no 3
+
+# n = int(input("give me the table of this number :"))
+# i = 1
+
+# while i <= 10: #stoping condition
+#     print(n * i)
+#     i += 1
+
+# q no 4
+
+# nums = [1,4,9, 16, 25, 36, 49,64,81, 100]
+
+# idx = 0
+
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+
