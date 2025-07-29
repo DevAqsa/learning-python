d8c23274e6f71f1dea729073e1d642171646c846
Aqsa Mumtaz <codewithaqsa@gmail.com>
2025-07-29 15:47:19 +0500

    loops learning

--- loops.py
+++ loops.py
@@ -22,7 +22,6 @@
 # q no 1 
 
 # i = 1
-
 # while i <= 100:
 #     print(i)
 #     i += 1
@@ -30,8 +29,7 @@
 # q no 2
 
 # i = 100
-
-# while i >=1:
+# while i >=1: stopping condition
 #     print(i)
 #     i -= 1
 
@@ -39,18 +37,80 @@
 
 # n = int(input("give me the table of this number :"))
 # i = 1
-
 # while i <= 10: #stoping condition
 #     print(n * i)
 #     i += 1
 
 # q no 4
 
-# nums = [1,4,9, 16, 25, 36, 49,64,81, 100]
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] #traverse
 
-# idx = 0
+# i = 0
+# while i < len(nums):
+#     print(nums[i]) #nums[0], nums[1], nums[2]...
+#     i += 1
 
-# while idx < len(nums):
-#     print(nums[idx])
-#     idx += 1
+# q no 5
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# x = 36
+# i = 0 #initialization
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found" ,i  )
+#         break
+#     else:
+#         print("findings...")
+#     i +=1
+
+# break and continue keyword
+
+# j = 1
+# while j <=5:
+#     print(j)
+#     if(j ==3):
+#         break
+#     j += 1
+
+# print("end of loop")    
+
+# i = 0
+# while i <= 5:
+#     if(i == 3):
+#         i += 1
+#         continue #skip
+#     print(i)
+#     i +=1
+
+# list = [1,2,3]
+
+# for val in list:
+#     print(val)
+
+
+# str = "aqsa mumtaz"
+
+# for chr in str:
+#     print(chr)
+# else: #we mostly use this else with break 
+#     print("end")
+
+# practice again questions
+# q no 1
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for val in nums:
+#     print(val)
+
+# q no 2 
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 49)
+x = 49
+
+idx = 0
+for el in nums:
+    if (el == x):
+        print("num found at idx: ", idx)
+    idx +=1
+
+
 
---
+++ range.py
@@ -0,0 +1,63 @@
+# range function
+
+# seq = range(10)
+
+# for i in range(10): range(stop)
+#     print(i)
+
+# for i in range(2, 10,): #range(start , stop)
+#     print(i)
+
+# for i in range(2, 10, 2): #range(start , stop, step)
+#     print(i)
+
+# for i in range(100, 0, -1): 
+#     print(i)
+
+# n = int(input("enter number: "))
+
+# for i in range(1, 11):
+#     print(n * i)
+
+# pass statement means null statement
+
+# for i in range(5):
+#     pass
+
+# print("love it")
+
+# practice questions
+
+# q no 1
+
+# n = 7
+
+# sum = 0
+# for i in range(1, n+1):
+#     sum +=i
+
+# print("total sum = :" ,sum)
+
+# ===============
+
+# n = 7
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum += i
+#     i += 1
+
+# print("total sum = :" ,sum)
+
+# q no 2
+
+# n = 5
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+
+# print("total factorial = :" , fact)
+
+
