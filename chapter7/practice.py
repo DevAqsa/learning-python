9b87ab39a58ad23ccbc7dfae2a5b5f55e3e8d072
Aqsa Mumtaz <codewithaqsa@gmail.com>
2025-09-04 16:21:00 +0500

    start OOP

--- chapter7/practice.py
+++ chapter7/practice.py
@@ -48,14 +48,28 @@
 
 # question 05
 
+# with open("demo.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+#     num = ""
+#     for i in range (len(data)):
+#         if (data[i] == ","):
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data[i]
+
+# altarnative way
+
+count = 0
 with open("demo.txt", "r") as f:
     data = f.read()
     print(data)
 
-    num = ""
-    for i in range (len(data)):
-        if (data[i] == ","):
-            print(num)
-            num = ""
-        else:
-            num += data[i]
+    nums = data.split(",")
+    for val in nums :
+        if(int(val) % 2 == 0):
+            count += 1
+
+print(count)
