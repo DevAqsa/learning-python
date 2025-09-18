9f3a6818a196bc9ed8d1a487ef116d38da841341
Aqsa <codewithaqsa@gmail.com>
2025-09-18 14:59:43 +0500

    oop

--- oop.py
+++ oop.py
@@ -1,1 +1,26 @@
- 
+#  procedural programming 
+
+# resueability --> functional prpgramming --> increase
+# redundancy --> oop --> decrease
+
+# oop --> class and object
+
+class Student : #always use the capistal letter for class name
+    name = "aqsa"
+    clr = "blue"
+
+o1 = Student() #how object created is this 
+print(o1.name)
+
+o2 = Student()
+print(o2.clr)
+
+# one more example
+
+# class Car:
+#     color = "purple"
+#     range = 233445
+
+# car1 = Car()
+# print(car1.color)
+# print(car1.range)
